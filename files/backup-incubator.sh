143d25d9fe5995b50935d2250ab60d8a16cd32e8
cip <ciprian.trusca@gmail.com>
2016-06-19 11:10:09 +0300

    fixed backup script

--- files/backup-incubator.sh
+++ files/backup-incubator.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 mkdir -p /tmp/aws
+chown -R incubator:incubator /tmp/aws
 
 now=$(date +'%Y%m%d%H%M')
 
 su -c "pg_dump production_db | gzip  > /tmp/aws/production-db-$now.gz" incubator
-tar -cpzf /tmp/aws/paperclip-$now.tar.gz /var/www/incubator107/shared/public/system
+tar -cpzf /tmp/aws/paperclip-$now.tar.gz /usr/share/nginx/incubator107/shared/public/system
 
 su -c "aws s3 cp /tmp/aws/production-db-$now.gz s3://incubator107/" incubator
 su -c "aws s3 cp /tmp/aws/paperclip-$now.tar.gz s3://incubator107/" incubator
 
 rm -rf /tmp/aws
-
